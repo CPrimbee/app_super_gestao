5f8bfb339cb54393d534047430e644105d5406e4
Cristiano Gonçalves <cprimbee@gmail.com>
2021-06-18 16:27:52 -0300

    Section8 - final part

--- database/seeds/FornecedorSeeder.php
+++ database/seeds/FornecedorSeeder.php
@@ -12,6 +12,6 @@ class FornecedorSeeder extends Seeder
      */
     public function run()
     {      
-        factory(SiteContato::class, 100)->create();        
+        factory(Fornecedor::class, 3)->create();        
     }
 }
