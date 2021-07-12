f365700a3f18191fca99e2de74bf731a0c51eb75
Cristiano Gonçalves <cprimbee@gmail.com>
2021-07-12 14:34:07 -0300

    Section9 - final part

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,31 +2,44 @@
 
 namespace App\Http\Controllers;
 
-use App\SiteContato;
 use Illuminate\Http\Request;
+use App\MotivoContato;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
     public function contato(Request $request){
 
-        $motivo_contatos = [
-            '1' => 'Dúvida',
-            '2' => 'Elogio',
-            '3' => 'Reclamação'
-        ];
+        $motivo_contatos = MotivoContato::all();
 
         return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request){
         //realizar a validação dos dados do formulário recebido no request
-        $request->validate([
-            'nome' => 'required|min:3|max:40',
+        $regras = [
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
             'telefone' => 'required',
-            'email' => 'required',
-            'motivo_contato' => 'required',
+            'email' => 'email',
+            'motivo_contatos_id' => 'required',
             'mensagem' => 'required|max:2000',
-        ]);
-        // SiteContato::create($request->all());
+        ];
+
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'nome.unique' => 'O nome informado já está em uso',
+
+            'email.email' => 'O email informado não é válido',
+
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000 caracteres',
+
+            'required' => 'O campo :attribute deve ser preenchido'
+        ];
+
+        $request->validate($regras, $feedback);
+        
+        SiteContato::create($request->all());
+        return redirect()->route('site.index');
     }
 }
